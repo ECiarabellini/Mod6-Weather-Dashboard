bbe68aad2c87b5183598d8b5ca030c5b626f55ad
Emily Ciarabellini <136280931+ECiarabellini@users.noreply.github.com>
2023-12-30 16:44:52 -0800

    Add logic for today in 5 day forecast

--- script.js
+++ script.js
@@ -10,8 +10,32 @@ var todayTemp = document.getElementById('today-temp');
 var todayWind = document.getElementById('today-wind');
 var todayHumidity = document.getElementById('today-humidity');
 var todayEmoji = document.getElementById('today-emoji');
-var coordinates = [47.6038321, -122.330062];
+var coordinates = [47.6038321, -122.330062]; //Seattle coordinates for testing purposes
 // var coordinates = [];
+let tZeroimg = document.querySelector("#tZero img");
+let tPlus1img = document.querySelector("#tPlus1 img");
+let tPlus2img = document.querySelector("#tPlus2 img");
+let tPlus3img = document.querySelector("#tPlus3 img");
+let tPlus4img = document.querySelector("#tPlus4 img");
+
+let tZerotemp = document.querySelector("#tZero .temp span");
+let tPlus1temp = document.querySelector("#tPlus1 .temp span");
+let tPlus2temp = document.querySelector("#tPlus2 .temp span");
+let tPlus3temp = document.querySelector("#tPlus3 .temp span");
+let tPlus4temp = document.querySelector("#tPlus4 .temp span");
+
+let tZerowind = document.querySelector("#tZero .wind span");
+let tPlus1wind = document.querySelector("#tPlus1 .wind span");
+let tPlus2wind = document.querySelector("#tPlus2 .wind span");
+let tPlus3wind = document.querySelector("#tPlus3 .wind span");
+let tPlus4wind = document.querySelector("#tPlus4 .wind span");
+
+
+let tZerohumid = document.querySelector("#tZero .humidity span");
+let tPlus1humid = document.querySelector("#tPlus1 .humidity span");
+let tPlus2humid = document.querySelector("#tPlus2 .humidity span");
+let tPlus3humid = document.querySelector("#tPlus3 .humidity span");
+let tPlus4humid = document.querySelector("#tPlus4 .humidity span");
 
 
 dateToday.textContent = dayjs().format('M/DD/YYYY');
@@ -52,10 +76,10 @@ function getWeather(city) {
         .then(function (data) {
             seletedState.textContent = data[0]["state"];
             selectedCountry.textContent = data[0]["country"];
-            console.log("lat: ", data[0]["lat"]);
-            console.log("lon: ", data[0]["lon"]);
+            // console.log("lat: ", data[0]["lat"]);
+            // console.log("lon: ", data[0]["lon"]);
             coordinates = [data[0]["lat"], data[0]["lon"]];  //latitude and longitude coordinates as an array
-            console.log('coordinates1: ', coordinates);
+            // console.log('coordinates1: ', coordinates);
         }
         ) 
     console.log('coordinates2: ', coordinates);
@@ -67,13 +91,44 @@ function getWeather(city) {
         })
         .then(function (data) {
             console.log("fiveDayForecast data: ", data);
-            console.log('date0:', data.list[0]['dt_txt']); //in UTC
-            console.log('date1:', data.list[1]['dt_txt']);
-
-            var formattedDate = new Date(data.list[0]['dt'] *1000); //convert unix timestamp to readible date in local time
-            console.log('date0: ', formattedDate);
-            // var formattedDate1 = new Date(dataOne *1000);
-            // console.log('date1: ', formattedDate1);
+            var todaysDate = new Date();
+            todaysDate.setHours(0); //set attributes to 0 to calculate days after correctly below
+            todaysDate.setMinutes(0); 
+            todaysDate.setSeconds(0); 
+            console.log('todaysDate: ', todaysDate);
+            let highTemp = 0;
+            for (let n = 0; n < data.list.length; n++){
+                var formattedDate = new Date(data.list[n]['dt'] *1000); //convert unix timestamp to readible date in local time
+                var dateDiff = (formattedDate - todaysDate)/86400000;  ///converted to days. less than 1 is today; 1.x is tomorrow, 2.x is day 3, etc.
+                var temp = data.list[n]['main']['temp'];
+                console.log('date', n, ': ', formattedDate,' _temp: ',temp, ' _wind:',data.list[n]['wind']['speed'],'_humid',data.list[n]['main']['humidity']);
+                console.log('highTemp: ', highTemp);
+                if (dateDiff < 1){
+                    if (highTemp < temp){
+                        highTemp = temp;
+                        console.log('temp ',temp);
+                        tZerotemp.textContent = highTemp;
+                        tZerowind.textContent = data.list[n]['wind']['speed'];
+                        tZerohumid.textContent = data.list[n]['main']['humidity'];
+                        let currentDayIcon = data.list[n]['weather'][0]['icon'];
+                        console.log(currentDayIcon);
+                        var dayimgURL = 'http://openweathermap.org/img/wn/' + currentDayIcon +'@2x.png'; 
+                        console.log('dayimgURL ', dayimgURL);
+                        tZeroimg.src = dayimgURL;
+                    }
+                }
+                if (dateDiff < 2){
+                    if (highTemp < temp){
+                        highTemp = temp;
+                        console.log('temp ',temp);
+                        tZerotemp.textContent = highTemp;
+                        tZerowind.textContent = data.list[n]['wind']['speed'];
+                        tZerohumid.textContent = data.list[n]['main']['humidity'];
+                    }
+                }
+            }
+            // console.log('date0:', data.list[0]['dt_txt']); //in UTC
+            
 
         });
 
@@ -85,9 +140,9 @@ function getWeather(city) {
         })
         .then(function (data) {
             console.log("current data: ", data);
-            todayTemp.textContent = data["main"]["temp"];
-            todayHumidity.textContent = data["main"]["humidity"];
+            todayTemp.textContent = data["main"]["temp"];            
             todayWind.textContent = data["wind"]["speed"];
+            todayHumidity.textContent = data["main"]["humidity"];
             let currentIcon = data['weather'][0]['icon']; 
             var imgURL = 'http://openweathermap.org/img/wn/' + currentIcon +'@2x.png';
             todayEmoji.src = imgURL;
@@ -117,7 +172,7 @@ var cardDates = document.querySelectorAll('.card-date');
 cardDates.forEach(function(cardDate, index) {
     // Calculate the date for each card (t+1, t+2, etc.)
     var futureDate = new Date(today);
-    futureDate.setDate(today.getDate() + index + 1);
+    futureDate.setDate(today.getDate() + index);
 
     // Format the date as MM/DD/YYYY
     var formattedDate = (futureDate.getMonth() + 1) + '/' + futureDate.getDate() + '/' + futureDate.getFullYear();
@@ -125,8 +180,6 @@ cardDates.forEach(function(cardDate, index) {
     // Set the text content of the card date
     cardDate.textContent = formattedDate;
 });
-
-
 
 ////////
 //-- need to figure out how to display high temp for each day (alternatively hardcode temp at certain time?? indexes vary based on when data is run). First temp of the day + index 4?? temp around noon?
